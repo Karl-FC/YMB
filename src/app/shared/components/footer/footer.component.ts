862ce25d6d95dd4c81dcf4a28f40befd5280981b
KFC <160381066+Karl-FC@users.noreply.github.com>
2024-06-07 19:03:11 +0800

    Added socials to Footer Adjusted Footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NewsletterFormComponent,CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
