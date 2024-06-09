67dfbda08657354f5e48e20c503d2309bf326db6
KFC <160381066+Karl-FC@users.noreply.github.com>
2024-06-09 22:11:22 +0800

    Injected Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { StartComponent } from './pages/start/start.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+
+import { Analytics } from '@angular/fire/analytics';
+import { Auth } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -15,5 +19,8 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private analytics = inject(Analytics);
+  private auth = inject(Auth);
+  private firestore = inject(Firestore);
   title = 'YMB';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,29 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { initializeApp,provideFirebaseApp } from "@angular/fire/app";
+import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCfg90nxHS6m4VjP-GbWTLCeEqhDXm6c3Y",
+    authDomain: "your-motivational-buddy.firebaseapp.com",
+    projectId: "your-motivational-buddy",
+    storageBucket: "your-motivational-buddy.appspot.com",
+    messagingSenderId: "757806310363",
+    appId: "1:757806310363:web:0e517795cf35284eae7dfe",
+    measurementId: "G-VBB8S8QWGV"
+  };
+  
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideRouter(routes),
+    provideHttpClient(), 
+    provideClientHydration()]
 };
+
