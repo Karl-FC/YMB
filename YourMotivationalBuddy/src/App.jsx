0091b0f75d2f69fedfae3270a4bb789ece5e590c
KFC <160381066+Karl-FC@users.noreply.github.com>
2024-10-18 18:58:44 +0800

    Install ReactJS

---
+++ YourMotivationalBuddy/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './Shared/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ YourMotivationalBuddy/src/Shared/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+
+    return(
+        <>
+        <h1>hello there</h1>
+        </>
+    );
+
+}
+
+export default Header
